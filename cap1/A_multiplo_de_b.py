5c0cb343ee80fe1d627209c77af4afdeae0be7b7
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-06-29 13:57:00 -0300

    Add files via upload

---
+++ cap1/A_multiplo_de_b.py
@@ -0,0 +1,12 @@
+def verifica_multiplo(a, b):
+    if a % b == 0:
+        print(f"{a} é multiplo de {b}")
+    else:
+        print(f"{a} não é multiplo de {b}")
+
+a = int(input("insira um valor para ser o A: "))
+b = int(input("insira um valor para ser o B: "))
+
+resultado = verifica_multiplo(a, b)
+
+print(resultado)
