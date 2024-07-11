57874db78e773cb14032667fba0f112348a4af19
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-07-11 00:27:44 -0300

    Add files via upload

---
+++ cap2/prime_list2.py
@@ -0,0 +1,11 @@
+# Lista de números primos entre 1 e 10
+prime_list = [2, 3, 5, 7]
+
+# Imprime a lista antes da adição
+print(f"Números primos: {prime_list}")
+
+# Adiciona o número 11 à lista usando o método append
+prime_list.append(11)
+
+# Imprime a lista após a adição
+print(f"Números primos após adição: {prime_list}")
