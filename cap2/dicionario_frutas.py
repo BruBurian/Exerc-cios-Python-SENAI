f6d78a45713d1f89bf32ae4b42af7eb617ed23f3
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-07-13 11:43:01 -0300

    Add files via upload

---
+++ cap2/dicionario_frutas.py
@@ -0,0 +1,15 @@
+frutas_dic = {
+    "maça" : 6000,
+    "melão" : 3000,
+    "banana" : 5000,
+    "laranja" : 4000
+}
+
+chaves_list = list(frutas_dic.keys())
+print("Chaves do dicionário: ", chaves_list)
+
+tem_maca = "maça" in frutas_dic
+tem__manga = "manga" in frutas_dic
+
+print("A chave maça está no dicionário?", tem_maca)
+print("A chave manga está no dicionário?", tem__manga)
