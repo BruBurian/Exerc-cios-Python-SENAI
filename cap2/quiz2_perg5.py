89e0001134d3a19d0e94f95aaec282830a746333
Bruno Nogueira Burian <151679524+BruBurian@users.noreply.github.com>
2024-07-15 20:39:28 -0300

    Add files via upload

---
+++ cap2/quiz2_perg5.py
@@ -0,0 +1,9 @@
+maria = {'coreano': 94, 'ingles': 91, 'matematica': 89, 'ciencia': 83}
+
+soma_notas = sum(maria.values())
+
+quantidade_notas = len(maria)
+
+media_notas = soma_notas / quantidade_notas
+
+print(f"A nota média de Maria é: {media_notas}")
